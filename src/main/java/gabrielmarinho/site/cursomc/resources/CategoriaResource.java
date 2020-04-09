80af3cf3f5a56ab411320ecb7c9af582d6c2ffa9
Gabriel Marinho <biel32771@gmail.com>
2020-04-09 18:39:00 -0300

    Listando toda as Categorias com DTO

--- src/main/java/gabrielmarinho/site/cursomc/resources/CategoriaResource.java
+++ src/main/java/gabrielmarinho/site/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package gabrielmarinho.site.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import gabrielmarinho.site.cursomc.domain.Categoria;
+import gabrielmarinho.site.cursomc.dto.CategoriaDTO;
 import gabrielmarinho.site.cursomc.services.CategoriaService;
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -24,10 +28,10 @@ public class CategoriaResource {
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
-		
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert (@RequestBody Categoria obj){
 		obj = service.insert(obj);
@@ -48,4 +52,13 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+
 }
--- src/main/java/gabrielmarinho/site/cursomc/services/CategoriaService.java
+++ src/main/java/gabrielmarinho/site/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package gabrielmarinho.site.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityExeption("Não e possivel excluir uma categoria que tem produtos");
 		}
+	}
+	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
